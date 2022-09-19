aaa7793d82272bf4c5e8136011ccae65b1a5c362
aadarsh805 <aadarshthakur80501@gmail.com>
2022-09-19 17:12:32 +0530

    async thunks, toolkit done, react-redux now

--- redux-toolkit-demo/index.js
+++ redux-toolkit-demo/index.js
@@ -1,6 +1,7 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions 
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
 
 console.log('Initial State: ', store.getState())
 
@@ -8,17 +9,19 @@ const unsubscribe = store.subscribe(() => {
     // console.log("Updated State: ", store.getState())
 })
 
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(19))
+store.dispatch(fetchUsers())
 
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.ordered())
-store.dispatch(icecreamActions.restocked(32))
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(19))
 
-unsubscribe()
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.ordered())
+// store.dispatch(icecreamActions.restocked(32))
+
+// unsubscribe()
